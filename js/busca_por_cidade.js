faf71690b92cf67147313f2c4eb1559545b279b0
Jhonatan Pinheiro Da Silva <jhonatan_pinheiro@outlook.com.br>
2021-02-14 13:19:26 -0300

    Desafio_Pokemon

--- js/busca_por_cidade.js
+++ js/busca_por_cidade.js
@@ -1,97 +1,147 @@
-        $(document).ready(function(){
-                $("#botao_buscar_pokemon").click(function(){
-                    var cidade = $("#input-cidade").val();
-                    var key = 'eed26403bd837bd4e1e09379c7a059ca';
-    
-                        $.ajax({
-                            url: 'http://api.openweathermap.org/data/2.5/weather',
-                            dataType: 'json',
-                            type: 'GET',
-                            data:  {lang:'pt_br', q:cidade, appid: key, units: 'metric'},
+$(document).ready(function () {
+    $("#botao_buscar_pokemon").click(function () {
+        var cidade = $("#input-cidade").val();
+        var key = 'eed26403bd837bd4e1e09379c7a059ca';
 
-                            success: function(data){
-                                console.log(data);
-                                $("#input-cidade").val('');
-                                var temperatura =  Math.round(data.main.temp);
-                                var tipoPokemon = '';
-                                var climaResultado = '';
+        $.ajax({
+            url: 'http://api.openweathermap.org/data/2.5/weather',
+            dataType: 'json',
+            type: 'GET',
+            data: { lang: 'pt_br', q: cidade, appid: key, units: 'metric' },
 
-                                $.each(data.weather,function(index,value){
-                                    climaResultado += '<p><strong>' + data.name + "<p>" + 
-                                     temperatura + '&deg;C' + ' | ' + value.description
-                                });
-                                $("#Aqui").html(climaResultado);
+            success: function (data) {
+                console.log(data);
 
-                                // Logica 
-                                if(temperatura < 5){
-                                    tipoPokemon = 'ice';
-                                }
-                              
-                                else if(temperatura >= 5 && temperatura < 10){
-                                    tipoPokemon = 'water';
-                                }
+                $("#input-cidade").val('');
+                var temperatura = Math.round(data.main.temp);
+                var climaChuva = data.weather[0].description;//------------->
+                console.log(climaChuva);//--------->
 
-                                else if(temperatura >= 12 && temperatura <= 15){
-                                    tipoPokemon = 'grass';
-                                }
 
-                                else if(temperatura >= 15 && temperatura < 21){
-                                    tipoPokemon = 'ground';
-                                }
+                var tipoPokemon = '';
+                var climaResultado = '';
 
-                                else if(temperatura >= 23 && temperatura < 27){
-                                    tipoPokemon = 'bug';
-                                }
 
-                                else if(temperatura >= 27 && temperatura < 33){
-                                    tipoPokemon = 'rock';
-                                }
 
-                                else if(temperatura > 33){
-                                    tipoPokemon = 'fire';
-                                }
+                $.each(data.weather, function (index, value) {
+                    climaResultado += '<p><strong>' + data.name + "<p>" +
+                        temperatura + '&deg;C' + ' | ' + value.description
+                });
+                console.log('Clima e Temperatura');
+                console.log(climaResultado);
+               
+                $("#resultado_clima_e_temperatura").html(climaResultado);
 
-                                else {
-                                    tipoPokemon = 'normal';
-                                }
-                              
-                                if(data){
+                // Logica 
+                if (temperatura < 5 && climaChuva !== 'chovendo' || climaChuva !== "Chovendo" ) {
+                    tipoPokemon = 'ice';
+                }
+
+                else if (temperatura >= 5 && temperatura < 10) {
+                    tipoPokemon = 'water';
+                }
+
+                else if (temperatura >= 12 && temperatura < 15) {
+                    tipoPokemon = 'grass';
+                }
+
+                else if (temperatura >= 15 && temperatura < 21) {
+                    tipoPokemon = 'ground';
+                }
+
+                else if (temperatura >= 23 && temperatura < 27) {
+                    tipoPokemon = 'bug';
+                }
+
+                else if (temperatura >= 27 && temperatura == 33) {
+                    tipoPokemon = 'rock';
+                }
+
+                else if (temperatura > 33) {
+                    tipoPokemon = 'fire';
+                }
+
+                else if (climaChuva == 'chovendo') { //------------->
+                    tipoPokemon = 'electric';
+                }
+
+                else {
+                    tipoPokemon = 'normal';
+                }
+
+                // console.log(climaChuva)
+                if (data) {
+                    $.ajax({
+                        url: `https://pokeapi.co/api/v2/type/${tipoPokemon}`,
+                        dataType: 'json',
+                        type: 'GET',
+
+                        success: function (data) {
+                            console.log(data);
+                            var arrayPokemons = data.pokemon;
+                            var numeroAleatorio = Math.floor(Math.random() * data.pokemon.length) + 1;
+
+                            //  console.log(numeroAleatorio);
+
+                            var pokemonEscolhidoAleatorio = arrayPokemons[numeroAleatorio].pokemon.name;
+                            var pokemonEscolhidoImg = arrayPokemons[numeroAleatorio].pokemon.url;
+
+                            console.log('array');
+                            console.log(arrayPokemons);
+
+                            console.log('pokemonEscolhido');
+                            console.log(pokemonEscolhidoAleatorio);
+
+                            console.log('pokemonEscolhidoImg');
+                            console.log(pokemonEscolhidoImg);
+
+                            console.log('NumeroAleatório');
+                            console.log(numeroAleatorio);
+
+
+                            //
+                            if(data){
+
                                     $.ajax({
-                                        url: `https://pokeapi.co/api/v2/type/${tipoPokemon}`,
+                                        url: `${pokemonEscolhidoImg}`,
                                         dataType: 'json',
                                         type: 'GET',
-                                                                                   
-                                        success: function(data){
+
+                                        
+
+                                        success: function (data) {
+
+                                            var caminhoUrl = data.sprites.front_default;
+
+                                            console.log('segundaData');
                                             console.log(data);
-                                            
-                                            var arrayPokemons = data.pokemon;
-                                            var numeroAleatorio = Math.floor(Math.random() * data.pokemon.length) + 1;
-                                            
-                                                // $.each(arrayPokemons,function(index,value){
-                                                //     console.log(value.pokemon.name);
-                                                                          
-                                                // });
-                                             console.log(numeroAleatorio);
-                                            
-                                             var pokemonEscolhidoAleatorio = arrayPokemons[numeroAleatorio].pokemon.name;
-                                 
-                                            $("#nome_do_pokemon").html(pokemonEscolhidoAleatorio);
-            
-                                            
-                                        } 
-                                    
+
+                                            console.log('VariavelUrl');
+                                            console.log(caminhoUrl);
+
+                                            $("#nome_do_pokemon").html("<span id='nome-do-pokemon'>" + pokemonEscolhidoAleatorio + "</span>");
+                                           
+                                            $("#div_img_do_pokemon").html("<img class='img-pokemon' src='" + caminhoUrl + "'>");
+
+                                        }    
+                                   
                                     });
-                                }
+                            }
+                            //
+                        }
 
-                            },
+                    });
+                }
 
-                            error: function(error) {
-                                console.log(error);
-                                $("#Aqui").html(error.responseJSON.message);
-                              }
-                        
-                        });
-    
-                });
-    
+            },
+
+            error: function (error) {
+                console.log(error);
+                $("#postando_resultado").html(error.responseJSON.message);
+            }
+
         });
+
+    });
+
+});
